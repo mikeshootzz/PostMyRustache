f7b8f0a45dbd9b9b8877a7b14dc3b55708c960c2
Mike <mike@ditton.ch>
2024-03-13 16:07:58 +0100

    added initial mock sql server from opensrv-mysql

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tokio::io::AsyncWrite;
+
+use opensrv_mysql::*;
+use tokio::net::TcpListener;
+
+struct Backend;
+
+#[async_trait::async_trait]
+impl<W: AsyncWrite + Send + Unpin> AsyncMysqlShim<W> for Backend {
+    type Error = io::Error;
+
+    async fn on_prepare<'a>(
+        &'a mut self,
+        _: &'a str,
+        info: StatementMetaWriter<'a, W>,
+    ) -> io::Result<()> {
+        info.reply(42, &[], &[]).await
+    }
+
+    async fn on_execute<'a>(
+        &'a mut self,
+        _: u32,
+        _: opensrv_mysql::ParamParser<'a>,
+        results: QueryResultWriter<'a, W>,
+    ) -> io::Result<()> {
+        results.completed(OkResponse::default()).await
+    }
+
+    async fn on_close(&mut self, _: u32) {}
+
+    async fn on_query<'a>(
+        &'a mut self,
+        sql: &'a str,
+        results: QueryResultWriter<'a, W>,
+    ) -> io::Result<()> {
+        println!("execute sql {:?}", sql);
+        results.start(&[]).await?.finish().await
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("0.0.0.0:3306").await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let (r, w) = stream.into_split();
+        tokio::spawn(async move { AsyncMysqlIntermediary::run_on(Backend, r, w).await });
+    }
 }
